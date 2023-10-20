5f341b87479ca4e9a8d4ceff3d230781073e7691
HaveToSleep <63563090+HaveToSleep@users.noreply.github.com>
2023-10-20 19:11:15 +0200

    added keys and changed responsive design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,21 +17,6 @@ const sites = [
     { name: "Our Philosophy", href: "philosophy"}
 ]
 
-const products = [
-    { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-    { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-    { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-    { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-    { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-    { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-    { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
-function classNames(...classes: any[]) {
-    return classes.filter(Boolean).join(' ')
-}
 
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -45,7 +30,7 @@ export default function Header() {
                         <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
                     </a>
                 </div>
-                <div className="flex lg:hidden">
+                <div className="flex sm:hidden">
                     <button
                         type="button"
                         className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -56,19 +41,65 @@ export default function Header() {
                     </button>
                 </div>
 
+                <div className="hidden sm:flex sm:flex-1 sm:justify-center">
                 {
                     sites.map((site) => (
-                        <a href="#" className="text-sm px-4 font-semibold leading-6 text-gray-900">
+                        <a key={site.name + "normal"} href="#" className="text-sm px-4 font-semibold leading-6 text-gray-900">
                              <Link to={site.href}>{site.name}</Link>
                         </a>
                     ))
                 }
+                </div>
 
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <div className="hidden sm:flex sm:flex-1 sm:justify-end">
                     <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                         Log in <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
+                <Dialog as="div" className="sm:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <div className="fixed inset-0 z-10" />
+                    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img
+                                    className="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                    alt=""
+                                />
+                            </a>
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {
+                                        sites.map((site) => (
+                                            <a key={site.name + "mobile"} href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                                <Link to={site.href}>{site.name}</Link>
+                                            </a>
+                                        ))
+                                    }
+                                </div>
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        Log in
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog.Panel>
+                </Dialog>
             </nav>
         </header>
     )
