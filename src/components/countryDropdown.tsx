18cc2842eec1b8a3bc5d3fa859413bda67fa2d70
Nikolaus Brissa <nbrissa@student.ethz.ch>
2023-10-21 21:59:47 +0200

    phil

--- src/components/countryDropdown.tsx
+++ src/components/countryDropdown.tsx
@@ -22,7 +22,7 @@ console.log(allCountries);
 export default function CountryDropdown() {
   const [country, setCountry] = useState("Options");
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left right-10">
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {country}
