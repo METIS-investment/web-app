3de3f464a46d38a4d59e36be297ce7c47565c61e
Nikolaus Brissa <nbrissa@student.ethz.ch>
2023-10-21 19:55:37 +0200

    fix mobile login, need to fix closing of thing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -92,10 +92,9 @@ export default function Header({login}: {login: boolean}) {
                                 </div>
                                 <div className="py-6">
                                     <a
-                                        href="#"
                                         className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                     >
-                                        Log in
+                                        <Link to="login">{login ? "Log out" : "Log in"}</Link>
                                     </a>
                                 </div>
                             </div>
