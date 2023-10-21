8be8405c0928999c57f3a91eeec24c4ac1a059c2
HaveToSleep <63563090+HaveToSleep@users.noreply.github.com>
2023-10-21 17:24:50 +0200

    continuing apply + favicon

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -1,5 +1,7 @@
 import LocationForm from "../components/application/locationForm";
 import {useState} from "react";
+import ValueForm from "../components/application/valueForm";
+import CompanyForm from "../components/application/companyForm";
 
 
 function Apply({login}: {login: boolean}) {
@@ -13,6 +15,8 @@ function Apply({login}: {login: boolean}) {
     street: "",
     city: "",
     zip: "",
+    companyname: "",
+    creationdate: "",
     assets: 0,
     liabilities: 0,
     profit: 0,
@@ -26,6 +30,7 @@ function Apply({login}: {login: boolean}) {
   const handleLocationSubmit = (fn: string, ln: string, ma: string, co  :string, st :string, ci :string, zi:string) => {
     setVals(previousState => {
       return { ...previousState,
+        step: 2,
         firstname: fn,
         lastname: ln,
         email: ma,
@@ -33,6 +38,16 @@ function Apply({login}: {login: boolean}) {
         street: st,
         city: ci,
         zip: zi
+      }
+    });
+  }
+
+  const handleCompanySubmit = (name: string, date: string) => {
+    setVals(previousState => {
+      return { ...previousState,
+        step: 3,
+        companyname: name,
+        creationdate: date
       }
     });
   }
@@ -58,8 +73,14 @@ function Apply({login}: {login: boolean}) {
         </div>
 
         <div className="flex-2 py-20">
-          <LocationForm submit = {handleLocationSubmit}/>
+          {
+            vals.step === 1 ?  <LocationForm submit = {handleLocationSubmit}/> :
+                vals.step === 2 ? <CompanyForm submit = {handleCompanySubmit}/> :
+                    <ValueForm />
+          }
+          {"step: " + vals.step + " "}
           {"" + vals.firstname + " " + vals.lastname + " " + vals.email + " " + vals.country + " " + vals.city + " " + vals.zip + " " + vals.street}
+          {" " + vals.creationdate + " " + vals.companyname}
         </div>
 
         <div className="flex-1 py-20">
