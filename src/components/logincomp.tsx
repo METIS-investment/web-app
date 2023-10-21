4595a8873a56d0181f12c5216fa4a91bc596d684
Nikolaus Brissa <nbrissa@student.ethz.ch>
2023-10-21 13:23:59 +0200

    login

--- src/components/logincomp.tsx
+++ src/components/logincomp.tsx
@@ -13,6 +13,15 @@
   ```
 */
 
+import React from 'react';
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { auth } from '../pages/firebase';
+
+const handleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+};
+
 
 export default function Example() {
     return (
@@ -26,15 +35,8 @@ export default function Example() {
           ```
         */}
         <div className="flex min-h-full flex-1 flex-col justify-center pt-20 px-8 py-30 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-     
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <div className="space-y-6">
               <div>
                 <img
                 className="mx-auto h-10 w-auto"
@@ -42,16 +44,18 @@ export default function Example() {
                 alt="Your Company"
               />
               </div>
-  
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            </div>
               <div>
                 <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                 onClick={handleSignIn}>Sign In with Google
                 </button>
               </div>
-            </form>
+            </div>
           </div>
         </div>
       </>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,18 @@
-import Example from "../components/logincomp";
+import React from 'react';
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { auth } from './firebase';
+import Example from '../components/logincomp';
+
+const handleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+};
 
 function Login() {
     return (
         <>
             <Example />
+            {/* <button onClick={handleSignIn}>Sign In with Google</button> */}
         </>
     );
 }
